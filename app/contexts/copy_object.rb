96fbc4296fcd0824b59346d071e68c24c8240efa
mdouchement <marc.douchement@predicsis.com>
2015-02-19 17:14:23 +0100

    Fix object copy, increase c arrierwave performance for large files

--- app/contexts/copy_object.rb
+++ app/contexts/copy_object.rb
@@ -1,4 +1,6 @@
 class CopyObject
+  TMP_COPY_DIR = File.join('tmp', 'copies')
+
   def self.call(source, uri, filename, bucket, key)
     CopyObject.new(source, uri, filename, bucket, key).call
   end
@@ -9,10 +11,18 @@ class CopyObject
 
   def call
     @s3_object = S3Object.find_by(uri: @uri) || S3Object.new(uri: @uri, bucket: @bucket, key: @key)
-    @s3_object.update_attributes(file: File.open(@source.file.path))
+    @s3_object.update_attributes(file: local_copy(@source.file.path))
     @s3_object.update_attributes(
         content_type: @source.content_type, size: File.size(@s3_object.file.path),
         md5: Digest::MD5.file(@s3_object.file.path).hexdigest)
     @s3_object
+  end
+
+  private
+
+  def local_copy(src)
+    FileUtils.mkdir_p(TMP_COPY_DIR)
+    FileUtils.cp(src, TMP_COPY_DIR)
+    File.open File.join(TMP_COPY_DIR, src.split(File::SEPARATOR).last)
   end
 end
